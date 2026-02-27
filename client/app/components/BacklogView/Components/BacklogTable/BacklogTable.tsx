40b4f9d9efd3d502f38d56a0219476725c32cfbf
Will Gray <willgraygames@gmail.com>
2026-02-26 23:22:36 -0500

    Added skeletons to the backlog table to placehold data as it loads

--- client/app/components/BacklogView/Components/BacklogTable/BacklogTable.tsx
+++ client/app/components/BacklogView/Components/BacklogTable/BacklogTable.tsx
@@ -1,4 +1,4 @@
-import { Anchor, Group, Table, Text, Image } from "@mantine/core";
+import { Anchor, Group, Table, Text, Image, Skeleton } from "@mantine/core";
 import type { BacklogEntry } from "../BacklogView/BacklogView";
 import styles from "./BacklogTable.module.scss";
 
@@ -6,46 +6,77 @@ type BacklogTableProps = {
     ownedGames: BacklogEntry[];
 }
 
-export default function BacklogTable (props: BacklogTableProps): React.JSX.Element {
+const defaultArray: BacklogEntry[] = Array(20).fill({
+    name: "",
+    appid: 0,
+    playtime_forever: 0,
+    rtime_last_played: 0,
+});
+
+export default function BacklogTable (props: BacklogTableProps) {
     const {
         ownedGames,
     } = props;
 
-    const rows = ownedGames.map((game) => {
-
-    return (
-      <Table.Tr color="#171a21" key={game.name}>
-        <Table.Td>
-            <Image src={`https://cdn.akamai.steamstatic.com/steam/apps/${game.appid}/header.jpg`} w={120} h={50} radius="md"/>
-        </Table.Td>
-        <Table.Td>
-          <Anchor fz="sm" href={`https://store.steampowered.com/app/${game.appid}`} target="_blank">
-            {game.name}
-          </Anchor>
-        </Table.Td>
-        <Table.Td>
-            <Text fz="sm" c="dimmed">
-                {game.basePrice ? `${game.basePrice}` : "No price data"}
-            </Text>
-        </Table.Td>
-        <Table.Td>
-          <Anchor component="button" fz="sm">
-            {game.appid}
-          </Anchor>
-        </Table.Td>
-        <Table.Td>
-            <Text fz="sm" c="dimmed">
-                {Math.floor(game.playtime_forever / 60)} hours {game.playtime_forever % 60} minutes
-            </Text>
-        </Table.Td>  
-        <Table.Td>
-            <Text fz="sm" c="dimmed">
-                {game.rtime_last_played ? new Date(game.rtime_last_played * 1000).toLocaleDateString() : "Never played"}
-            </Text>
-        </Table.Td>
-      </Table.Tr>
-    );
-  });
+    const setRows = (rowSet: BacklogEntry[]) => 
+        rowSet.map((game, index) => {
+        if (game && typeof game === "object" && "basePrice" in game && game.basePrice) {
+                    return (
+            <Table.Tr color="#171a21" key={game.appid}>
+                <Table.Td>
+                    <Image src={`https://cdn.akamai.steamstatic.com/steam/apps/${game.appid}/header.jpg`} w={120} h={50} radius="md"/>
+                </Table.Td>
+                <Table.Td>
+                <Anchor fz="sm" href={`https://store.steampowered.com/app/${game.appid}`} target="_blank">
+                    {game.name}
+                </Anchor>
+                </Table.Td>
+                <Table.Td>
+                    <Text fz="sm" c="dimmed">
+                        {game.basePrice ? `${game.basePrice}` : "No price data"}
+                    </Text>
+                </Table.Td>
+                <Table.Td>
+                <Anchor component="button" fz="sm">
+                    {game.appid}
+                </Anchor>
+                </Table.Td>
+                <Table.Td>
+                    <Text fz="sm" c="dimmed">
+                        {Math.floor(game.playtime_forever / 60)} hours {game.playtime_forever % 60} minutes
+                    </Text>
+                </Table.Td>  
+                <Table.Td>
+                    <Text fz="sm" c="dimmed">
+                        {game.rtime_last_played ? new Date(game.rtime_last_played * 1000).toLocaleDateString() : "Never played"}
+                    </Text>
+                </Table.Td>
+            </Table.Tr>
+            );
+        } else {
+            return (<Table.Tr color="#171a21" key={index}>
+                <Table.Td>
+                    <Skeleton height={50} width={120} radius="md" />
+                </Table.Td>
+                <Table.Td>
+                    <Skeleton height={15} width={'100%'} radius="md" />
+                </Table.Td>
+                <Table.Td>
+                    <Skeleton height={15} width={'100%'} radius="md" />
+                </Table.Td>
+                <Table.Td>
+                    <Skeleton height={15} width={'100%'} radius="md" />
+                </Table.Td>
+                <Table.Td>
+                    <Skeleton height={15} width={'100%'} radius="md" />
+                </Table.Td>  
+                <Table.Td>
+                    <Skeleton height={15} width={'100%'} radius="md" />
+                </Table.Td>
+            </Table.Tr>)
+        }
+    
+});
 
 
   return (<div className={styles["backlog-wrapper"]}>
@@ -61,7 +92,7 @@ export default function BacklogTable (props: BacklogTableProps): React.JSX.Eleme
             <Table.Th>Last Played</Table.Th>
           </Table.Tr>
         </Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
+        <Table.Tbody>{setRows(ownedGames.length > 0 ? ownedGames : defaultArray)}</Table.Tbody>
       </Table>
     </Table.ScrollContainer>
   </div>);
